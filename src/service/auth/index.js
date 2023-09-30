ee21e57fc13e15337c36b85b0e7e233830620e12
Ismoil Aburahmonov <ismoil4432@gmail.com>
2023-09-30 13:13:05 +0500

    updated

--- src/service/auth/index.js
+++ src/service/auth/index.js
@@ -2,6 +2,6 @@ import axios from '../axios'
 
 export const useAuth = {
   admin_login: async (data) => await axios.post('/auth/admin/login', { ...data }),
-  admin_verify: async (data) => await axios.post('/auth/otp/verify', { ...data }),
-  client_login: async (data) => await axios.post('/auth/admin/login', { ...data })
+  client_login: async (data) => await axios.post('/auth/client/login', { ...data }),
+  otp_verify: async (data) => await axios.post('/auth/otp/verify', { ...data })
 }
--- src/stores/auth/auth.js
+++ src/stores/auth/auth.js
@@ -5,25 +5,36 @@ import { useAuth } from '@/service/auth'
 export const useAuthStore = defineStore('auth', () => {
   const user = reactive({
     data: [],
-    email: ''
+    email: '',
+    role: '',
+    token: ''
   })
 
   const LOGIN = async (data) => {
     user.email = data.email
-    const res = await useAuth.admin_login(data)
+    user.role = data.role
+    localStorage.setItem('email', data.email)
+    localStorage.setItem('role', data.role)
+    const res =
+      data.role === 'client' ? await useAuth.client_login(data) : await useAuth.admin_login(data)
     return res
   }
 
   const VERIFY = async (data) => {
-    console.log(data);
-    const res = await useAuth.admin_verify(data)
+    console.log(data)
+    const res = await useAuth.otp_verify(data)
     return res
   }
 
-  const GET = async () => {
-  }
+  const GET = async () => {}
 
   const DATA = computed(() => chats.data)
 
-  return { LOGIN, GET, VERIFY, DATA }
+  const GET_EMAIL = computed(() => user.email || localStorage.getItem('email'))
+
+  const GET_ROLE = computed(() => user.role || localStorage.getItem('role'))
+
+  const GET_TOKEN = computed(() => user.token || localStorage.getItem('token'))
+
+  return { LOGIN, GET, VERIFY, DATA, GET_EMAIL, GET_ROLE, GET_TOKEN }
 })
